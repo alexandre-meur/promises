b994abb269075a4df35d1236508cd736a3812151
Alexandre MEUR <alex.meur@gmail.com>
2019-06-20 10:04:02 +0200

    Utilisation de async/await

--- index.js
+++ index.js
@@ -3,41 +3,30 @@ let pathTemp = 'temp/';
 let pathPubs = 'temp/pubs.json';
 
 //Si le dossier temp existe on le supprime
-fs.pathExists(pathTemp)
-    .then(exists => {
-        if (exists) {
-            return fs.remove(pathTemp)
-                .then(() => console.log(pathTemp, ' removed'))
-                .catch(() => console.log('error while removing'));
+async function doThings(){
+    try{
+        let exists = await fs.pathExists(pathTemp);
+        if(exists){
+            await fs.remove(pathTemp);
+            console.log(pathTemp, ' removed');
         }
-        return;
-    })
-    //On crée le dossier
-    .then(() => {
-        return fs.ensureDir(pathTemp)
-            .then(() => console.log('temp/ created'))
-            .catch(() => console.log('temp/ was not created'));
-    })
-    //On lit le fichier contenant les pubs
-    .then(() => {
-        return fs.readJSON('./pubsData.json')
-            .then(json => {
-                //On écrit le fichier tmp/pubs.json
-                return fs.writeJson(pathPubs, json)
-                    .then(() => console.log('pubs.json writen !'))
-                    .catch(() => console.log('couldn\'t write json'));
-
-            })
-            .catch(() => console.log('couldn\'t read Json'));
-    })
-    //Et on écout les modifs sur le fichier
-    .then(
+        await fs.ensureDir(pathTemp);
+        let json = await fs.readJSON('./pubsData.json');
+        await fs.writeJson(pathPubs, json);
         fs.watchFile(pathPubs, (curr, prev) => {
             console.log(`the current mtime is: ${curr.mtime}`);
             console.log(`the previous mtime was: ${prev.mtime}`);
         })
-    )
-    .catch(() => console.log('failure pathExists'));
+    }
+    catch(error){
+        console.log(error);
+    }
+
+}
+
+doThings();
+
+
 
 
 
